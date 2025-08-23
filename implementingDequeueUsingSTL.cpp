e2a72c808d79d912546d410f9f6722fe48bec6d9
Mehak Garg <missgunnu1@gmail.com>
2025-08-23 21:24:39 +0530

    Implementing deque using STL

---
+++ implementingDequeueUsingSTL.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+int main(){
+deque<int>d;
+d.push_back(10);
+d.push_front(30);
+cout << d.front() << endl;
+d.pop_front();
+cout << d.front() << endl;
+cout << d.back() << endl;
+}
